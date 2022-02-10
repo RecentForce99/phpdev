438f431b9707471822726e8f50baad4ae7e177bc
RecentForce99 <76057851+RecentForce99@users.noreply.github.com>
2022-02-11 00:46:47 +0300

    Update index.php

--- war/index.php
+++ war/index.php
@@ -1,242 +1,279 @@
 <?php
 abstract class Component
 {
-protected $parent;
+    protected $parent;
 
-public function setParent(Component $parent)
-{
-$this->parent = $parent;
-}
+    public function setParent(Component $parent)
+    {
+        $this->parent = $parent;
+    }
 
-public function getParent(): Component
-{
-return $this->parent;
-}
+    public function getParent(): Component
+    {
+        return $this->parent;
+    }
 
-public function add(Component $component): void { }
+    public function add(Component $component): void { }
 
-public function remove(Component $component): void { }
-
-
-public function isComposite(): bool
-{
-return false;
-}
+    public function remove(Component $component): void { }
 
 
-abstract public function operation();
+    public function isComposite(): bool
+    {
+        return false;
+    }
+
+
+    abstract public function operation();
 }
 interface Unit
 {
-public static function createArmy();
+    public static function createArmy();
 }
 
 class calculate
 {
-public static function calc_army_damage_health (array $army, array $standartArmy, bool $rain, bool $ice) : array
-{
-$damage = 0;
-$health = 0;
-$pehota = $standartArmy['pehota'];
-$luchniki = $standartArmy['luchniki'];
-$konnica = $standartArmy['konnica'];
+    public static function calc_army_damage_health (array $army, array $standartArmy, bool $rain, bool $ice) : array
+    {
+        $damage = 0;
+        $health = 0;
+        $pehota = $standartArmy['pehota'];
+        $luchniki = $standartArmy['luchniki'];
+        $konnica = $standartArmy['konnica'];
 
-($rain == true) ? $luchniki = $luchniki['damage'] / 2 : $luchniki;
+        ($rain == true) ? $luchniki = $luchniki['damage'] / 2 : $luchniki;
 
-foreach ($army['units'] as $unit => $count)
-{
-$damage += ${$unit}['damage'] * $count;
+        foreach ($army['units'] as $unit => $count)
+        {
+            $damage += ${$unit}['damage'] * $count;
 
-if($ice == true) $health += ${$unit}['health'] * $count;
-elseif($ice == false) $health += ${$unit}['health'] * $count + ${$unit}['armour'] * $count;
+            if($ice == true) $health += ${$unit}['health'] * $count;
+            elseif($ice == false) $health += ${$unit}['health'] * $count + ${$unit}['armour'] * $count;
+
+        }
+        return ['damage' => $damage, 'health' => $health];
+    }
 
 }
-return ['damage' => $damage, 'health' => $health];
-}
 
-}
+
 
 class Aleksandr extends Component implements Unit
 {
 
-public static function createArmy()
-{
-return [
-'name' => 'Александр Ярославич',
-'units' => [
-'pehota' => 200,
-'luchniki' => 30,
-'konnica' => 15,
-]
-];
-}
-public function operation()
-{
-
-
-
-}
-}
-
-$standartArmy =
-[
-'pehota' => [
-'health' => 100,
-'armour' => 10,
-'damage' => 10,
-],
-'luchniki' => [
-'health' => 100,
-'armour' => 5,
-'damage' => 20,
-],
-'konnica' => [
-'health' => 300,
-'armour' => 30,
-'damage' => 30,
-]
-];
-class Ulf extends Component
-{
-public static function createArmy()
-{
-return [
-'name' => 'Ульф Фасе',
-'units' => [
-'pehota' => 90,
-'luchniki' => 65,
-'konnica' => 25,
-]
-];
-}
-
-public function operation()
-{
-
-}
-}
-
-
-class Composite extends Component
-{
-
-protected $children;
-
-public function __construct()
-{
-$this->children = new \SplObjectStorage();
-}
-
-
-public function add(Component $component): void
-{
-$this->children->attach($component);
-$component->setParent($this);
-}
-
-public function remove(Component $component): void
-{
-$this->children->detach($component);
-$component->setParent(null);
-}
-
-public function isComposite(): bool
-{
-return true;
-}
-
-
-public function operation(): string
-{
-$results = [];
-foreach ($this->children as $child) {
-$results[] = $child->operation();
-}
-
-return "Branch(" . implode("+", $results) . ")";
-}
-}
-
-$health = [
-calculate::calc_army_damage_health(Aleksandr::createArmy(), $standartArmy, true, true),
-calculate::calc_army_damage_health(Ulf::createArmy(), $standartArmy, true, true)
-];
-
-print_r($health);
-function clientCode(Component $component, $standartArmy, $rain, $ice)
-{
-$army = [Aleksandr::createArmy(), Ulf::createArmy()];
-$healthAndDamage = [
-calculate::calc_army_damage_health(Aleksandr::createArmy(), $standartArmy, $rain, $ice),
-calculate::calc_army_damage_health(Ulf::createArmy(), $standartArmy, $rain, $ice)
-];
-
-echo "<table border='1'>
-    <tr>
-        <th></th>
-        <th>".$army[0]['name']."</th>
-        <th>".$army[1]['name']."</th>
-    </tr>
-    <tr>
-        <th>Army units:</th>
-        <td>unit1 (count), unit2(count), ...</td>
-        <td>unit1 (count), unit2(count), ...</td>
-    </tr>
-    <tr>
-        <th>Погибшие</th>
-        <td>".$healthAndDamage[0]['health']."</td>
-        <td>".$healthAndDamage[1]['health']."</td>
-    </tr>
-    <tr>
-        <th>Выжившие</th>
-        <td>".$healthAndDamage[0]['health']."</td>
-        <td>".$healthAndDamage[1]['health']."</td>
-    </tr>";
-
-    $duration = 0;
-    while ($healthAndDamage[0]['health'] >= 0 && $healthAndDamage[1]['health'] >= 0)
+    public static function createArmy()
     {
-    $healthAndDamage[0]['health'] -= $healthAndDamage[0]['damage'];
-    $healthAndDamage[1]['health'] -= $healthAndDamage[0]['damage'];
-    $duration++;
+        return [
+            'name' => 'Александр Ярославич',
+            'units' => [
+                'pehota' => 200,
+                'luchniki' => 30,
+                'konnica' => 15,
+            ]
+        ];
+    }
+    public function operation()
+    {
+        $army = [Aleksandr::createArmy(), Ulf::createArmy()];
+
+        //Для удобства
+        $armyLiving = [
+            'pehota' => $army[0]['units']['pehota'] - $army[1]['units']['pehota'],
+            'luchniki' => $army[0]['units']['luchniki'] - $army[1]['units']['luchniki'],
+            'konnica' => $army[0]['units']['konnica'] - $army[1]['units']['konnica'],
+        ];
+
+        //Для удобства
+        $armyDead = [
+            'pehota' => abs($army[0]['units']['pehota']) - abs($armyLiving['pehota']),
+            'luchniki' => abs($army[0]['units']['luchniki']) - abs($armyLiving['luchniki']),
+            'konnica' => abs($army[0]['units']['konnica']) - abs($armyLiving['konnica']),
+        ];
+
+        //Удаляем отрицательные числа и заменяем нулями
+        foreach ($armyLiving as $key => $val)
+        {
+            if($armyLiving[$key] <= 0) $armyLiving[$key] = 0;
+            if($armyDead[$key] <= 0) $armyDead[$key] = 0;
+        }
+
+        //Выжившие
+        foreach ($armyLiving as $key => $val)
+        {
+            $armyLivingSum += abs($val);
+        }
+
+        //Погибшие
+        foreach ($armyDead as $key => $val)
+        {
+           $armyDeadSum += abs($val);
+        }
+
+        $result = [
+            'armyLiving' => $armyLiving,
+            'armyDead' => $armyDead,
+            'armyLivingSum' => $armyLivingSum,
+            'armyDeadSum' => $armyDeadSum
+            ];
+
+
+        return $result;
+    }
+}
+
+
+class Ulf extends Component implements Unit
+{
+    public static function createArmy()
+    {
+        return [
+            'name' => 'Ульф Фасе',
+            'units' => [
+                'pehota' => 90,
+                'luchniki' => 65,
+                'konnica' => 25,
+            ]
+        ];
     }
 
-    echo " <tr>
-        <th>Health after $duration hits:</th>
-        <td>" . $healthAndDamage[0]['health'] . "</td>
-        <td>" . $healthAndDamage[1]['health'] . "</td>
-    </tr>
-    <tr>
-        <th>Result</th>
-        <td>" . ($healthAndDamage[0]['health'] > $healthAndDamage[1]['health'] ? 'WINNER' : 'LOOSER') . "</td>
-        <td>" . ($healthAndDamage[1]['health'] > $healthAndDamage[0]['health'] ? 'WINNER' : 'LOOSER') . "</td>
-    </tr>
-</table>";
+    public function operation()
+    {
+
+        $army = [Aleksandr::createArmy(), Ulf::createArmy()];
+        //Для удобства
+        $armyLiving = [
+            'pehota' => $army[1]['units']['pehota'] - $army[0]['units']['pehota'],
+            'luchniki' => $army[1]['units']['luchniki'] - $army[0]['units']['luchniki'],
+            'konnica' => $army[1]['units']['konnica'] - $army[0]['units']['konnica'],
+        ];
+
+        //Для удобства
+        $armyDead = [
+            'pehota' => abs($army[1]['units']['pehota']) - abs($armyLiving['pehota']),
+            'luchniki' => abs($army[1]['units']['luchniki']) - abs($armyLiving['luchniki']),
+            'konnica' => abs($army[1]['units']['konnica']) - abs($armyLiving['konnica']),
+        ];
+
+        //Удаляем отрицательные числа и заменяем нулями
+        foreach ($armyLiving as $key => $val)
+        {
+            if($armyLiving[$key] <= 0) $armyLiving[$key] = 0;
+            if($armyDead[$key] <= 0) $armyDead[$key] = 0;
+        }
+
+        //Выжившие
+        foreach ($armyLiving as $key => $val)
+        {
+           $armyLivingSum += abs($val);
+        }
+
+        //Погибшие
+        foreach ($armyDead as $key => $val)
+        {
+           $armyDeadSum += abs($val);
+        }
+
+        $result = [
+            'armyLiving' => $armyLiving,
+            'armyDead' => $armyDead,
+            'armyLivingSum' => $armyLivingSum,
+            'armyDeadSum' => $armyDeadSum
+        ];
+
+        return $result;
+    }
+}
 
 
-//print_r($component->operation());
+
+function clientCode(Component $component, $standartArmy, $rain, $ice)
+{
+    $Aleksandr = new Aleksandr();
+    $Ulf = new Ulf();
+    $army = [Aleksandr::createArmy(), Ulf::createArmy()];
+    $healthAndDamage = [
+        calculate::calc_army_damage_health(Aleksandr::createArmy(), $standartArmy, $rain, $ice),
+        calculate::calc_army_damage_health(Ulf::createArmy(), $standartArmy, $rain, $ice)
+    ];
+
+        $armyDead1 = $Aleksandr->operation()['armyDead'];
+        $armyDead2 = $Ulf->operation()['armyDead'];
+        $armyDeadSum1 = $Aleksandr->operation()['armyDeadSum'];
+        $armyDeadSum2 = $Ulf->operation()['armyDeadSum'];
+        $armyLiving1 = $Aleksandr->operation()['armyLiving'];
+        $armyLiving2 = $Ulf->operation()['armyLiving'];
+        $armyLivingSum1 = $Aleksandr->operation()['armyLivingSum'];
+        $armyLivingSum2 = $Ulf->operation()['armyLivingSum'];
+
+
+    echo "<table border='1'>
+            <tr>
+            <th></th>
+            <th>".$army[0]['name']."</th>
+            <th>".$army[1]['name']."</th>
+        </tr>
+        <tr>
+            <th>Army units:</th>
+            <td>Пехота: ".$army[0]['units']['pehota'].", Лучники: ".$army[0]['units']['luchniki'].", Конница: ".$army[0]['units']['konnica']."</td>
+            <td>Пехота: ".$army[1]['units']['pehota'].", Лучники: ".$army[1]['units']['luchniki'].", Конница: ".$army[1]['units']['konnica']."</td>
+        </tr>
+        <tr>";
+
+        $duration = 0;
+        while ($healthAndDamage[0]['health'] >= 0 && $healthAndDamage[1]['health'] >= 0)
+        {
+            $healthAndDamage[0]['health'] -= $healthAndDamage[0]['damage'];
+            $healthAndDamage[1]['health'] -= $healthAndDamage[0]['damage'];
+
+            $duration++;
+        }
+
+        echo " 
+            <th>Погибшие</th>
+            <td>Пехота: ".$armyDead1['pehota'].", Лучники: ".$armyDead1['luchniki'].", Конница: ".$armyDead1['konnica'].", Всего: ".$armyDeadSum1."</td>
+            <td>Пехота: ".$armyDead2['pehota'].", Лучники: ".$armyDead2['luchniki'].", Конница: ".$armyDead2['konnica'].", Всего: ".$armyDeadSum2."</td>
+        </tr>
+        <tr>
+            <th>Выжившие</th>
+            <td>Пехота: ".$armyLiving1['pehota'].", Лучники: ".$armyLiving1['luchniki'].", Конница: ".$armyLiving1['konnica'].", Всего: ".$armyLivingSum1."</td>
+            <td>Пехота: ".$armyLiving2['pehota'].", Лучники: ".$armyLiving2['luchniki'].", Конница: ".$armyLiving2['konnica'].", Всего: ".$armyLivingSum2."</td>
+        </tr>
+        
+            <tr>
+            <th>Health after $duration hits:</th>
+            <td>" . $healthAndDamage[0]['health'] . "</td>
+            <td>" . $healthAndDamage[1]['health'] . "</td>
+        </tr>
+        <tr>
+            <th>Result</th>
+            <td>" . ($healthAndDamage[0]['health'] > $healthAndDamage[1]['health'] ? 'WIN' : 'LOST') . "</td>
+            <td>" . ($healthAndDamage[1]['health'] > $healthAndDamage[0]['health'] ? 'WIN' : 'LOST') . "</td>
+        </tr>
+        </table>";
+
+
 
 }
-$army = [
-'army1' => [
-'name' => 'Александр Ярославич',
-'units' => [
-'pehota' => 200,
-'luchniki' => 30,
-'konnica' => 15,
-]
-],
-'army2' => [
-'name' => 'Ульф Фасе',
-'units' => [
-'pehota' => 90,
-'luchniki' => 65,
-'konnica' => 25,
-]
-]
-];
-$simple = new Aleksandr();
-//print_r(calculate::calc_army_damage_health(Aleksandr::createArmy(), $standartArmy, true, false));
+$standartArmy =
+    [
+        'pehota' => [
+            'health' => 100,
+            'armour' => 10,
+            'damage' => 10,
+        ],
+        'luchniki' => [
+            'health' => 100,
+            'armour' => 5,
+            'damage' => 20,
+        ],
+        'konnica' => [
+            'health' => 300,
+            'armour' => 30,
+            'damage' => 30,
+        ]
+    ];
 
-//$simple->operation();
-clientCode($simple, $standartArmy, false, true);
+$composite = new Aleksandr();
+clientCode($composite, $standartArmy, false, true);
+
+
